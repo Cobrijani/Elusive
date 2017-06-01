7e9b1ecb689158b095f108c933dc52fee633ecb4
Stefan <cobrijani@gmail.com>
2017-06-01 12:04:17 +0200

    Added user for logstash

--- deployment/elk/start.sh
+++ deployment/elk/start.sh
@@ -181,6 +181,21 @@ if [ "$ELASTICSEARCH_START" -ne "1" ] && [ "$LOGSTASH_START" -ne "1" ] \
   exit 1
 fi
 
+#Add role that is configured for logstash indexer named logstash_writer
+curl -k \
+   --user elastic:changeme \
+   -X POST \
+   -H "Content-Type: application/json"\
+   -d "{ \"cluster\": [\"manage_index_templates\", \"monitor\"], \"indices\": [{ \"names\": [\"logstash-*\", \"filebeat-*\", \"winlogbeat-*\"], \"privileges\": [\"write\", \"delete\", \"create_index\"]}]}" \
+   https://localhost:9200/_xpack/security/role/logstash_writer
+
+# Add user with role logstash writer
+curl -k \
+   --user elastic:changeme \
+   -X POST \
+   -H "Content-Type: application/json"\
+   -d "{ \"password\" : \"changeme\",\"roles\" : [ \"logstash_writer\"],\"full_name\" : \"Internal Logstash User\"}"\
+   https://localhost:9200/_xpack/security/user/logstash_internal
+
 touch $OUTPUT_LOGFILES
-tail -f $OUTPUT_LOGFILES &
-wait
+tail -f $OUTPUT_LOGFILES & wait
--- deployment/filebeat/start.sh
+++ deployment/filebeat/start.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-./wait.sh elk:9200 #wait for elasticsearch
-./wait.sh elk:5044 # wait for logstash
-curl -k --user elastic:changeme -XPUT 'https://elk:9200/_template/filebeat?pretty' -d@/etc/filebeat/filebeat.template.json
-#/etc/init.d/filebeat start
+./wait.sh -t 30 -h elk -p 9200 #wait for elasticsearch
+./wait.sh -t 15 -h elk -p 5044 # wait for logstash
+curl -k \
+ --user elastic:changeme \
+ -X PUT \
+ 'https://elk:9200/_template/filebeat?pretty' \
+ -d@/etc/filebeat/filebeat.template.json
 service filebeat start
 mkdir /var/log/filebeat
 touch /var/log/filebeat/filebeat.log
