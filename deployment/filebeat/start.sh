b5a197a78a72abce471aaf541f289b5b15243208
Stefan <cobrijani@gmail.com>
2017-06-05 22:22:15 +0200

    Added readme

--- deployment/filebeat/start.sh
+++ deployment/filebeat/start.sh
@@ -1,11 +1,15 @@
-#!/bin/bash
+#!/bin/sh
+
+./wait.sh -t 300 -h es -p 9200 #wait for elasticsearch
+
+
 #Add role that is configured for logstash indexer named logstash_writer
 curl -k \
    --user elastic:changeme \
    -X POST \
    -H "Content-Type: application/json"\
    -d "{ \"cluster\": [\"manage_index_templates\", \"monitor\"], \"indices\": [{ \"names\": [\"logstash-*\", \"filebeat-*\", \"winlogbeat-*\"], \"privileges\": [\"write\", \"delete\", \"create_index\"]}]}" \
-   https://localhost:9200/_xpack/security/role/logstash_writer
+   https://es:9200/_xpack/security/role/logstash_writer
 
 # Add user with role logstash writer
 curl -k \
@@ -13,4 +17,16 @@ curl -k \
    -X POST \
    -H "Content-Type: application/json"\
    -d "{ \"password\" : \"changeme\",\"roles\" : [ \"logstash_writer\"],\"full_name\" : \"Internal Logstash User\"}"\
-   https://localhost:9200/_xpack/security/user/logstash_internal
+   https://es:9200/_xpack/security/user/logstash_internal
+
+curl -k \
+ --user elastic:changeme \
+ -X PUT \
+ 'https://es:9200/_template/filebeat?pretty' \
+ -d@/etc/filebeat/filebeat.template.json
+./wait.sh -t 15 -h logstash -p 5044 # wait for logstash
+/etc/init.d/filebeat start -e
+
+mkdir /var/log/filebeat
+touch /var/log/filebeat/filebeat.log
+tail -f /var/log/filebeat/filebeat.log & wait
