600478018bc5bf940bd908f4caa2e89d242c4b3a
Milorad Vojnovic <milekuglas@gmail.com>
2017-06-06 10:28:36 +0200

    Logstash pattern fix

---
+++ deployment/configure.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Add role that is configured for logstash indexer named logstash_writer
+curl -k \
+   --user elastic:changeme \
+   -X POST \
+   -H "Content-Type: application/json"\
+   -d "{ \"cluster\": [\"manage_index_templates\", \"monitor\"], \"indices\": [{ \"names\": [\"logstash-*\", \"filebeat-*\", \"winlogbeat-*\"], \"privileges\": [\"write\", \"delete\", \"create_index\"]}]}" \
+   https://localhost:9200/_xpack/security/role/logstash_writer
+
+# Add user with role logstash writer
+curl -k \
+   --user elastic:changeme \
+   -X POST \
+   -H "Content-Type: application/json"\
+   -d "{ \"password\" : \"changeme\",\"roles\" : [ \"logstash_writer\"],\"full_name\" : \"Internal Logstash User\"}"\
+   https://localhost:9200/_xpack/security/user/logstash_internal
+
+curl -k \
+ --user elastic:changeme \
+ -X PUT \
+ 'https://localhost:9200/_template/filebeat?pretty' \
+ -d@/etc/filebeat/filebeat.template.json
