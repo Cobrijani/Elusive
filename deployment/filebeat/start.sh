405e46874717fcda4889dd0d59ca649f262818ae
Stefan <cobrijani@gmail.com>
2017-06-09 17:03:07 +0200

    Fixed indexes and added firewall rule

--- deployment/filebeat/start.sh
+++ deployment/filebeat/start.sh
@@ -3,27 +3,30 @@
 ./wait.sh -t 300 -h es -p 9200 #wait for elasticsearch
 ./wait.sh -t 15 -h logstash -p 5044 # wait for logstash
 
+ES_USER=elastic
+ES_PASS=changeme
+
 #Add role that is configured for logstash indexer named logstash_writer
 curl -k \
-   --user elastic:changeme \
+   --user ${ES_USER}:${ES_PASS} \
    -X POST \
    -H "Content-Type: application/json"\
-   -d "{ \"cluster\": [\"manage_index_templates\", \"monitor\"], \"indices\": [{ \"names\": [\"logstash-*\", \"filebeat-*\", \"winlogbeat-*\"], \"privileges\": [\"write\", \"delete\", \"create_index\"]}]}" \
+   -d "{ \"cluster\": [\"manage_index_templates\", \"monitor\"], \"indices\": [{ \"names\": [\"logstash-*\", \"filebeat-*\", \"winlogbeat-*\", \"appbeat-*\", \"apachebeat-*\", \"firebeat-*\"], \"privileges\": [\"write\", \"delete\", \"create_index\"]}]}" \
    https://es:9200/_xpack/security/role/logstash_writer
 
 # Add user with role logstash writer
 curl -k \
-   --user elastic:changeme \
+   --user ${ES_USER}:${ES_PASS} \
    -X POST \
    -H "Content-Type: application/json"\
    -d "{ \"password\" : \"changeme\",\"roles\" : [ \"logstash_writer\"],\"full_name\" : \"Internal Logstash User\"}"\
    https://es:9200/_xpack/security/user/logstash_internal
 
-curl -k \
- --user elastic:changeme \
- -X PUT \
- 'https://es:9200/_template/filebeat?pretty' \
- -d@/etc/filebeat/filebeat.template.json
+# curl -k \
+#  --user ${ES_USER}:${ES_PASS} \
+#  -X PUT \
+#  "https://es:9200/_template/${INDEX_NAME}?pretty" \
+#  -d@/usr/share/filebeat/filebeat.template.json
 
 /etc/init.d/filebeat start -e
 
--- documents/Firewall rules.md
+++ documents/Firewall rules.md
@@ -2,4 +2,5 @@
 
 This document contain alerts that are specified to occur on certain condition.
 
-1. If in n timeframe x REJECTS ON SAME IP ADDRESS OCCURS BAN IP ADDRESS
+1. If in n timeframe x REJECTS ON SAME IP ADDRESS OCCURS
+2. If in n timeframe x DROPS ON SAME IP ADDRESS OCCURS
