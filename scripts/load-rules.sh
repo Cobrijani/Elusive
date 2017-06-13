e76a9b966055e5beab50a03854b0c246a8be684a
Stefan <cobrijani@gmail.com>
2017-06-13 16:17:03 +0200

    Added windows event log rules

--- scripts/load-rules.sh
+++ scripts/load-rules.sh
@@ -11,11 +11,11 @@ ES_PASS=changeme
 ES_URL=localhost
 ES_PORT=9200
 
+echo "Loading rules in elasticsearch..."
 for filename in ./../documents/*/*.json; do
   [ -e "$filename" ] || continue
 
   base=${filename##*/}
-  echo curl${base%.json}
 
   curl -k \
   --user ${ES_USER}:${ES_PASS} \
@@ -24,3 +24,5 @@ for filename in ./../documents/*/*.json; do
   -d@${filename}
 
 done
+
+echo "Done."
