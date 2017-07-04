0c412430c881a27d0237714c7939d133bee4d417
Stefan <cobrijani@gmail.com>
2017-07-04 20:37:38 +0200

    Changed atm generator for more convenient case.

--- log_generator/generators/atm_gen.py
+++ log_generator/generators/atm_gen.py
@@ -10,23 +10,39 @@ class AtmGen(Generator):
 
     self.atm_ids = ['atm-1', 'atm-2', 'atm-3', 'atm-4', 'atm-5']
     self.credit_cards = ['kartica-1', 'kartica-2', 'kartica-3', 'kartica-4', 'kartica-5']
+    self.combination_table = {
+      'kartica-1': [self.atm_ids[0]],
+      'kartica-2': [self.atm_ids[1]],
+      'kartica-3': [self.atm_ids[2]],
+      'kartica-4': [self.atm_ids[3]],
+      'kartica-5': self.atm_ids
+    }
 
     self.withdraw_amount = random.randrange(500, 20000, 500)
 
     self.logs = [self.__create_limit_reached_log, self.__create_with_draw_log]
 
+  def __generate_combination(self):
+    credit_card = np.random.choice(self.credit_cards)
+    atm = np.random.choice(self.combination_table[credit_card])
+    withdraw = np.random.choice(self.withdraw_amount)
+
+    return credit_card, withdraw, atm
+
   def __create_with_draw_log(self):
+    c, w, d = self.__generate_combination()
     return 'INFO r.a.u.f.c.AtmService:200 - Credit card: {}, withdraw amount: {}, ATM id: {}, result: Success!'.format(
-      np.random.choice(self.credit_cards),
-      np.random.choice(self.withdraw_amount),
-      np.random.choice(self.atm_ids)
+      c,
+      w,
+      d
     )
 
   def __create_limit_reached_log(self):
+    c, w, d = self.__generate_combination()
     return 'WARN r.a.u.f.c.AtmService:60 - Credit card: {}, withdraw amount: {}, ATM id: {}, result: Limit reached!'.format(
-      np.random.choice(self.credit_cards),
-      np.random.choice(self.withdraw_amount),
-      np.random.choice(self.atm_ids)
+      c,
+      w,
+      d
     )
 
   def generate_log_row(self):
